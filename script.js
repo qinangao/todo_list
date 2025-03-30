6f39eb6e999bd2cf577bbb637573aaaa53f8dab7
Qinan Gao <qinanagao@gmail.com>
2025-03-30 21:19:26 +0100

    add eventlistener for edit task

--- script.js
+++ script.js
@@ -12,13 +12,16 @@ form.addEventListener("submit", function (e) {
   }
   //Add taaks:
   const addTask = function () {
-    let html = `<li><button class="check__btn uncheck"></button>${inputTask.value}<span class="delete__btn">&times;</span></li>`;
+    let html = `<li><button class="check__btn uncheck"></button><p class="text">${inputTask.value}</p>
+    <button class="edit__btn">Edit</button>
+    <span class="delete__btn">&times;</span></li>`;
     list.insertAdjacentHTML("beforeend", html);
     inputTask.value = "";
   };
   addTask();
 });
 
+//Handle check and delete
 list.addEventListener("click", function (e) {
   //Check tasks
   if (e.target.classList.contains("check__btn")) {
@@ -28,5 +31,22 @@ list.addEventListener("click", function (e) {
   //Delete tasks
   if (e.target.classList.contains("delete__btn")) {
     e.target.closest("li").remove();
+  }
+  //Edit tasks
+  if (e.target.classList.contains("edit__btn")) {
+    let taskText = e.target.previousElementSibling;
+    if (taskText.isContentEditable) {
+      if (taskText.innerText.trim() === "") {
+        alert("Task cannot be emply");
+        taskText.focus();
+      } else {
+        taskText.setAttribute("contenteditable", "false");
+        e.target.innerText = "Edit";
+      }
+    } else {
+      taskText.setAttribute("contenteditable", "true");
+      taskText.focus();
+      e.target.innerText = "Save";
+    }
   }
 });
