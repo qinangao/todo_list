bab45ecd4f1514640415793c38f730184aca9c96
Qinan Gao <qinanagao@gmail.com>
2025-03-31 23:27:22 +0100

    add setLocalStorage

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const inputTask = document.querySelector(".input__task");
 const form = document.querySelector("#form");
 const list = document.querySelector("ul");
 
+//Set local storage
+let tasksData = JSON.parse(localStorage.getItem("todo")) || [];
+const setLocalStorage = function () {
+  localStorage.setItem("todo", JSON.stringify(tasksData));
+};
+
+//Handle submit
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   //Alert if input is emply
@@ -10,7 +17,12 @@ form.addEventListener("submit", function (e) {
     alert("Input can not be emply");
     return;
   }
-  //Add taaks:
+  const taskObj = {
+    text: inputTask.value,
+    completed: false,
+  };
+
+  //Add tasks:
   const addTask = function () {
     let html = `<li><button class="check__btn uncheck"></button><p class="text">${inputTask.value}</p>
     <button class="edit__btn">Edit</button>
@@ -18,19 +30,42 @@ form.addEventListener("submit", function (e) {
     list.insertAdjacentHTML("beforeend", html);
     inputTask.value = "";
   };
+  tasksData.push(taskObj);
   addTask();
+  setLocalStorage();
 });
+//Render local storage tasks
+const renderTasks = function () {
+  list.innerHTML = "";
+  tasksData.forEach((task, index) => {
+    let html = `<li data-index="${index}"  ${
+      task.completed ? 'class="completed"' : ""
+    }>
+  <button class="check__btn ${task.completed ? "checked" : "uncheck"}"></button>
+  <p class="text">${task.text}</p>
+  <button class="edit__btn">Edit</button>
+  <span class="delete__btn">&times;</span>
+</li>`;
+    list.insertAdjacentHTML("beforeend", html);
+  });
+};
+renderTasks();
 
 //Handle check and delete
 list.addEventListener("click", function (e) {
+  const taskItem = e.target.closest("li");
+  const taskIndex = Number(taskItem.dataset.index);
   //Check tasks
   if (e.target.classList.contains("check__btn")) {
+    tasksData[taskIndex].completed = !tasksData[taskIndex].completed;
+    console.log(taskItem);
     e.target.classList.toggle("checked");
-    e.target.closest("li").classList.toggle("completed");
+    taskItem.classList.toggle("completed");
+    setLocalStorage();
   }
   //Delete tasks
   if (e.target.classList.contains("delete__btn")) {
-    e.target.closest("li").remove();
+    taskItem.remove();
   }
   //Edit tasks
   if (e.target.classList.contains("edit__btn")) {
@@ -42,11 +77,13 @@ list.addEventListener("click", function (e) {
       } else {
         taskText.setAttribute("contenteditable", "false");
         e.target.innerText = "Edit";
+        setLocalStorage();
       }
     } else {
       taskText.setAttribute("contenteditable", "true");
       taskText.focus();
       e.target.innerText = "Save";
+      setLocalStorage();
     }
   }
 });
